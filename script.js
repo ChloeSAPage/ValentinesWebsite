915da7a82ef55277e46f900d68689b404401d9fa
ChloeSAPage <chloe.s.a.page@Gmail.com>
2024-02-03 05:35:23 +0000

    added no button functionality

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const question = document.querySelector('.question');
 const yesBtn = document.querySelector('.yes-btn');
 const noBtn = document.querySelector('.no-btn');
+const wrapper = document.querySelector('.wrapper');
+
+const wrapperRect = wrapper.getBoundingClientRect();
+const noBtnRect = noBtn.getBoundingClientRect();
+
 
 yesBtn.addEventListener('click', () => {
     question.innerHTML = 'Yay! I Love You!';
+});
+
+noBtn.addEventListener('mouseover', () => {
+    const width = Math.floor(Math.random() * (wrapperRect.width
+        - noBtnRect.width)) + 1;
+
+    const height = Math.floor(Math.random() * (wrapperRect.height
+        - noBtnRect.height)) + 1;
+
+    noBtn.style.left = width + 'px';
+    noBtn.style.top = height + 'px';
 });
